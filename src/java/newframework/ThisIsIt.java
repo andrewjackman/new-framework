f1ed80e6744663e2e3feef231df71994af76f16b
Andrew Jackman <andrew@andrewjackman.com>
2014-06-08 22:11:46 +0100

    All done :)

---
+++ src/java/newframework/ThisIsIt.java
@@ -0,0 +1,10 @@
+class NewFramework {
+    public static void main(String[] args) {
+        new <Insert Your Class Here>
+
+        // Now do the same for the rest of your project. It's that simple.
+        // Oh, and you need to think a bit about what you're doing. Maybe some
+        // context classes etc. But use your brian, that's the key to success
+        // with this amazing framework.
+    }
+}
